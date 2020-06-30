1dfaf80a276fb5058b6d11c5f2f3c18986cde5a2
Bryan Almaraz <bryan.almaraz96@gmail.com>
2020-07-01 00:56:45 +0800

    added navbar icons, back btn on expense item

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import hamIcon from '../assets/media/open-menu.png'
+import plane from '../assets/media/mail.svg'
 
 function Navbar() {
   return (
-    <div className="flex flex-row justify-between px-8 py-4">
-      <h1 className="text-2xl"><Link to="/">Trip Wallet</Link></h1>
-      <div className="ham-icon">
+    <div className="flex flex-row justify-between items-center px-8 py-4">
+      <h1 className="text-2xl font-light flex flex-row items-center">
+        <div className="w-8 pr-1 relative">
+          <img src={plane} alt="logo" />
+        </div>
+        <Link to="/">tripwallet</Link>
+      </h1>
+      <div className="ham-icon bg-gray-800 rounded-full p-2 cursor-pointer">
         <img className="h-8 w-9" src={hamIcon} alt="toggle menu" />
       </div>
     </div>
--- client/src/components/pages/ExpenseItem.js
+++ client/src/components/pages/ExpenseItem.js
@@ -28,39 +28,42 @@ function ExpenseItem({ data, setExpenseData }) {
 
   // TODO: back to trip button
   return (
-    <div className="px-8 rounded-lg bg-white max-w-sm m-auto mb-4 mt-3 shadow-lg">
-      <div className="flex flex-row justify-start items-start border-b py-4">
-        <h2 className="pr-4 text-2xl">
-          <span className="text-3xl font-thin">{expenseItem.foreignPrice}</span>
-          <span className="pl-1 text-xs">{foreignCurrency}</span>
-        </h2>
-        <h2 className="text-2xl">
-          <span className="text-3xl font-thin">{expenseItem.baseCurrencyPrice}</span>
-          <span className="pl-1 text-xs">{baseCurrency}</span>
-        </h2>
-      </div>
-      <h3 className=" pt-4">
-        <span className="font-semibold pr-1">Name:</span>
-        <span className="font-light">{expenseItem.expenseName}</span>
-      </h3>
-      <h3 className="my-1">
-        <span className="font-semibold pr-1">Category:</span>
-        <span className="font-light">{expenseItem.category}</span>
-      </h3>
-      {expenseItem.spread > 0 && <h3 className="py-1">{expenseItem.spread}</h3>}
-      <h3 className="my-1">
-        <span className="font-semibold pr-1">Start Date:</span>
-        <span className="font-light">{createDMYDate(expenseItem.startDate)}</span>
-      </h3>
-      <h3 className="my-1">{expenseItem.endDate && createDMYDate(expenseItem.endDate)}</h3>
-
-      <div className="flex flex-row justify-between items-end py-4">
-        <div className="w-8 cursor-pointer" onClick={() => setDelete(!unlockDelete)}>
-          <img src={lockIcon} alt="unlock delete button" />
+    <div>
+      <div className="px-8 rounded-lg bg-white max-w-sm m-auto mb-4 mt-3 shadow-lg">
+        <div className="flex flex-row justify-start items-start border-b py-4">
+          <h2 className="pr-4 text-2xl">
+            <span className="text-3xl font-thin">{expenseItem.foreignPrice}</span>
+            <span className="pl-1 text-xs">{foreignCurrency}</span>
+          </h2>
+          <h2 className="text-2xl">
+            <span className="text-3xl font-thin">{expenseItem.baseCurrencyPrice}</span>
+            <span className="pl-1 text-xs">{baseCurrency}</span>
+          </h2>
         </div>
-        {unlockDelete && <button className="hover:underline bg-red-600 text-white py-1 px-2 rounded-lg cursor-pointer" onClick={handleDelete}>Delete Expense</button>}
-        <Link to="/trip/expenseform" className="hover:underline py-1 px-2" onClick={handleEditExpenseRedirect}>Edit Expense</Link>
+        <h3 className=" pt-4">
+          <span className="font-semibold pr-1">Name:</span>
+          <span className="font-light">{expenseItem.expenseName}</span>
+        </h3>
+        <h3 className="my-1">
+          <span className="font-semibold pr-1">Category:</span>
+          <span className="font-light">{expenseItem.category}</span>
+        </h3>
+        {expenseItem.spread > 0 && <h3 className="py-1">{expenseItem.spread}</h3>}
+        <h3 className="my-1">
+          <span className="font-semibold pr-1">Start Date:</span>
+          <span className="font-light">{createDMYDate(expenseItem.startDate)}</span>
+        </h3>
+        <h3 className="my-1">{expenseItem.endDate && createDMYDate(expenseItem.endDate)}</h3>
+
+        <div className="flex flex-row justify-between items-end py-4">
+          <div className="w-8 cursor-pointer" onClick={() => setDelete(!unlockDelete)}>
+            <img src={lockIcon} alt="unlock delete button" />
+          </div>
+          {unlockDelete && <button className="hover:underline bg-red-600 text-white py-1 px-2 rounded-lg cursor-pointer" onClick={handleDelete}>Delete Expense</button>}
+          <Link to="/trip/expenseform" className="hover:underline py-1 px-2" onClick={handleEditExpenseRedirect}>Edit Expense</Link>
+        </div>
       </div>
+      <Link className="mx-10 mt-4 block px-6 py-3 rounded-lg bg-gray-800 text-white " to="/trip">Back To Trip</Link>
     </div>
   )
 }
